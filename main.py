2162ba8e97756197683ba48f78136735124388b7
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2022-01-14 03:43:57 +0330

    tests updated.

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ import TsDIct
 
 if __name__ == '__main__':
     di = TsDIct.TsDict()
-    di.put(1,2)
     di.put(1, 2)
     di.put(1, 2)
-    di.put(1, 2)
+    di.put(8, 3)
+    di.put(2, 3)
+    di.put('1', 3)
+    di.put(4, 3)
+    di.put('2', 3)
+    di.put('alice', 3)
+
+    di.put(1, ['ali', 'jason'])
     print(di.list)
+    print(di)
+    a = dict()
+    a['1'] = 2
+    print(a)
+    a['1'] = 3
+    a[1] = 3
+    a[5] = 3
+
+    print(a)
